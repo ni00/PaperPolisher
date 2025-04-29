26f64ff90887349ccb466cfc191100d23b63b2d9
ni00 <mail@nixiaobai.com>
2025-04-29 20:35:25 +0800

    feat: refactor App component and add MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import { Button } from "./components/ui/button";
 import "./App.css";
+import MainPage from "./page/main-page";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
+  // const [greetMsg, setGreetMsg] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name: "jack" }));
-  }
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   setGreetMsg(await invoke("greet", { name: "jack" }));
+  // }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-full bg-gray-100">
-      <h1>Welcome to Tauri + React + Tailwind + Shadui!</h1>
-      <Button onClick={greet}>Greet</Button>
-      <p>{greetMsg}</p>
-      <p className="mt-4 text-gray-600">This is a simple Tauri application.</p>
+    <div>
+      <MainPage />
     </div>
   );
 }
