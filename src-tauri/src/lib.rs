77533429213936807835d4f731e5251c727ebf81
ni00 <mail@nixiaobai.com>
2025-05-01 20:07:26 +0800

    fix: remove unused invoke_handler from Tauri run function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,6 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
