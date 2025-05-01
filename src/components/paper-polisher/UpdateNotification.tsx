1a2b986fbb8b47148663eb3b50a7c67cc69a6787
ni00 <mail@nixiaobai.com>
2025-05-01 21:15:53 +0800

    refactor: remove unused React import from UpdateNotification component

--- src/components/paper-polisher/UpdateNotification.tsx
+++ src/components/paper-polisher/UpdateNotification.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
 import { checkUpdate, UpdateInfo } from '@/lib/utils';
 
 interface UpdateNotificationProps {
